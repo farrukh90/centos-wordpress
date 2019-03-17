7110f3276e342eccd1117faeb1128d980c19d34f
farrukh90 <farrukhsadykov@gmail.com>
2019-03-17 16:54:40 -0500

    '

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ pipeline{
     stages{
         stage("run packer"){
             steps{
-                sh "packer build -var "WORDPRESS_VERSION=5.0.4" image.json"
+                sh 'packer build -var "WORDPRESS_VERSION=5.0.4" image.json'
             }
         }
     }
